11830651d43da1999101982a4f2d5408b20efb37
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-04 21:54:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import streamlit as st
-import random
+import secrets
 import string
+import pyperclip
 
 def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for i in range(length))
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
     return password
 
 st.title("Password Generator")
-length = st.slider("Select Password Length", 8, 128, 12)
-password = generate_password(length)
-st.write("Your generated password is:", password)
+
+length = st.slider("Select password length:", min_value=8, max_value=32, value=12, step=1)
+
+if st.button("Generate Password"):
+    password = generate_password(length)
+    st.write("Generated Password:", password)
+    if st.button("Copy Password"):
+        pyperclip.copy(password)
+        st.success("Password copied to clipboard!")

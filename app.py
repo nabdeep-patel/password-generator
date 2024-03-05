7c46fee337bc0ae8babfa34901316200e08265a8
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-05 06:15:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import secrets
 import string
-import clipboard
 
 def generate_password(length):
     alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    password = ''.join(secrets.choice(alphabet) for _ in range(length))
     return password
 
 st.title("Password Generator")
@@ -14,7 +13,5 @@ length = st.slider("Select password length:", min_value=8, max_value=32, value=1
 
 if st.button("Generate Password"):
     password = generate_password(length)
-    st.write("Generated Password:", password)
-    if st.button("Copy Password"):
-        clipboard.copy(password)
-        st.success("Password copied to clipboard!")
+    st.write("Generated Password:")
+    st.text_area("Password", value=password, height=1, key="password", disabled=True)
